d0c6e2252650e11ca03b3326ea77391bab6f607f
Danejw <djw.learn@gmail.com>
2020-11-25 12:18:03 -0800

    added fmod audio to football scene

---
+++ CsInVR XR Project/Assets/FmodCollisionSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+namespace CSInVR.FMOD
+{
+    public class FmodCollisionSound : MonoBehaviour
+    {
+        [SerializeField] private string eventPath;
+
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            RuntimeManager.PlayOneShotAttached(eventPath, this.gameObject);
+        }
+    }
+}
