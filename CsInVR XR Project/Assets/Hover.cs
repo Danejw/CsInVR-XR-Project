2f6cf8885f95f54b4ed6620bdfcb68cbaa57fdd1
Danejw <djw.learn@gmail.com>
2020-12-14 14:12:15 -0800

    fixed bugs in loop
    created yard lines
    changed anit aliasing settings
    edited blocker to attacker mode

---
+++ CsInVR XR Project/Assets/Hover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CSInVR.Football
+{
+    public class Hover : MonoBehaviour
+    {
+        //Inputs
+        public float degreesPerSecond = 15;
+        public float amplitude = .5f;
+        public float frequency = 1;
+
+        // position storage variables
+        Vector3 posOffset = new Vector3();
+        Vector3 tempPos = new Vector3();
+
+        // use this for initialization
+        private void Start()
+        {
+            posOffset = transform.position;
+        }
+
+        private void Update()
+        {
+            // spin object in the Y axis
+            transform.Rotate(new Vector3(0, Time.deltaTime * degreesPerSecond, 0), Space.World);
+
+            // float up/dpwn with Sin()
+            tempPos = posOffset;
+            tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+            transform.position = tempPos;
+        }
+    }
+}
